41857e1f24af0e7f3ecab43d3c15cfc4d28076d5
Anano Topuria <anano.topuriaa@gmail.com>
2022-12-25 17:44:04 +0400

    smooth scrolling-event propagation

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 'use strict';
-
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector(`.btn--scroll-to`);
+const section1 = document.querySelector(`#section--1`);
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -29,3 +30,106 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////////
+// Button scrolling
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log(`Current scroll (x/y)`, window.pageXOffset, window.pageYOffset);
+  console.log(
+    `height/width viewport`,
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: `smooth`,
+  // });
+
+  section1.scrollIntoView({ behavior: `smooth` });
+});
+
+////////////////////////////////////
+//Page navigation
+
+// document.querySelectorAll(`.nav__link`).forEach(function (el) {
+//   el.addEventListener(`click`, function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute(`href`);
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: `smooth` });
+//   });
+// });
+
+// 1. Add even listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
+  e.preventDefault();
+  // Matching stategy
+  if (e.target.classList.contains(`nav__link`)) {
+    const id = e.target.getAttribute(`href`);
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: `smooth` });
+  }
+});
+
+////////////////////////////////////
+/*
+const header = document.querySelector(`.header`);
+const allSelections = document.querySelectorAll(`.selection`);
+console.log(allSelections);
+
+document.getElementById(`section--1`);
+const allButtons = document.getElementsByTagName(`button`);
+console.log(allButtons);
+
+const message = document.createElement(`div`);
+message.classList.add(`cookie-message`);
+
+message.textContent = `We use cookies for improved functionality and analytics`;
+message.innerHTML = `We use cookies for improved functionality and analytics <button class="btn btn--close-cookie">Got it!  </button>`;
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+document
+  .querySelector(`.btn--close-cookie`)
+  .addEventListener(`click`, function () {
+    message.remove();
+  });
+
+// Styles
+
+message.style.backgroundColor = `#37383d`;
+message.style.width = `120%`;
+
+document.documentElement.style.setProperty(`--color-primary`, `orangered`);
+
+
+
+
+
+const h1 = document.querySelector(`h1`);
+h1.addEventListener(`mouseenter`, function (e) {
+  alert(`addEventListener: Great! You are reading the heading`);
+});
+
+h1.onmouseenter = function (e) {
+  alert(`onmouseenter: Great! You are reading the heading`);
+};
+*/
