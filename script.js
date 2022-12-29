61bb080dc06d9289d0ceee94f306a00c30ce398d
Anano Topuria <anano.topuriaa@gmail.com>
2022-12-29 11:07:30 +0400

    sticky navigation

--- script.js
+++ script.js
@@ -131,7 +131,44 @@ const handleHover = function (e, opacity) {
 nav.addEventListener(`mouseover`, handleHover.bind(0.5));
 nav.addEventListener(`mouseout`, handleHover.bind(1));
 
+// // Sticky navigation
+// const initialCoords = section1.getBoundingClientRect();
+// window.addEventListener(`scroll`, function () {
+//   console.log(window.scrollY);
+
+//   if (window.scrollY > initialCoords.top) nav.classList.add(`sticky`);
+//   else nav.classList.remove(`sticky`);
+// });
+
 ////////////////////////////////////
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+const header = document.querySelector(`.header`);
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add(`sticky`);
+  else nav.classList.remove(`sticky`);
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
 /*
 const header = document.querySelector(`.header`);
 const allSelections = document.querySelectorAll(`.selection`);
